80f60cb688507d7c11a4d79f235797df1b5b759a
PawelKline <pavelkuklinweb@gmail.com>
2023-04-02 11:23:41 +0500

    add: API function from yandex translater

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', ()=> {
         const finalResultField = document.querySelector('.output--text');
         const getResultBtn = document.querySelector('.button_get-js');
         const resultData = {};
+        const translateJs = document.querySelector('.button_copy-js')
 
 
         const createNewField = (e) => {
@@ -40,7 +41,7 @@ document.addEventListener('DOMContentLoaded', ()=> {
         }
 
         const createEspData = (newData) => {
-            resultData["Esp"] = {};
+            resultData["es"] = {};
             
             console.log('Тут будет английский перевод')
         }
@@ -63,6 +64,34 @@ document.addEventListener('DOMContentLoaded', ()=> {
 
             finalResultField.innerHTML = `${newsData}`
         }
+
+        translateJs.addEventListener('click', (event) => {
+            event.preventDefault();
+            const data = 'Сильный';
+          
+            const getTranslate = (data) => {
+              return fetch(`https://translate.yandex.net/api/v1.5/tr.json/translate`, {
+                method: 'POST',
+                body: `key=trnsl.1.1.20200303T153320Z.bcb87af6074aebbf.be4bdba1de9cec25a20bfc6409687922086791e9&lang=ru-en&text=${data}`,
+                headers: {
+                  "Content-type": "application/x-www-form-urlencoded"
+                },
+              });
+            };
+            getTranslate(data).then((response) => {
+          
+              if (response.status !== 200) {
+                throw new Error('error');
+              }
+              return response.json();
+            }).then((result) => {
+                finalResultField.textContent = result.text.toString().toLowerCase();
+          
+            }).catch((error) => {
+              console.log(error);
+            });
+          
+          });
 
         newField.addEventListener('click', createNewField)
         getResultBtn.addEventListener('click', createData)
